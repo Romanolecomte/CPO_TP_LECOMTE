73111a0d2bc0ad0400a5484f0340c84129442773
Romain Lecomte <romainlecomte@epfedufr>
2022-10-30 18:00:29 +0100

    creation de la classe Tartiflette dans le tp2 Manip

---
+++ TP2_manip_LECOMTE/src/tp2_manip_lecomte/Tartiflette.java
@@ -0,0 +1,16 @@
+package tp2_manip_lecomte;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author romai
+ */
+public class Tartiflette { // on creer une classe 
+    int nbCalories;
+    public Tartiflette(int nbCalories2) { // analyse l'attribut nbCalories
+    nbCalories = nbCalories2;
+}}
